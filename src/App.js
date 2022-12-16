1a99d84e894ce9ec19c1cde46ffbcb1e0d274c8b
zooyaho <jiwoo1268@naver.com>
2022-12-16 17:35:15 +0900

    feat: initial setting

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import BasicForm from "./components/BasicForm";
+import SimpleInput from "./components/SimpleInput";
+import Parent from "./Parent";
+
+function App() {
+  return (
+    <div className="app">
+      <SimpleInput />
+      {/* <BasicForm /> */}
+      {/* <Parent /> */}
+    </div>
+  );
+}
+
+export default App;
